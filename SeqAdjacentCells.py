20664b951dda10fdb2fdbb0ce3ed82b304285d4c
TonyDC <up201305244@gcloud.fe.up.pt>
2018-04-20 12:51:47 +0100

    Comments added

--- SeqAdjacentCells.py
+++ SeqAdjacentCells.py
@@ -13,7 +13,10 @@ ACTIVE_ELEMENT = 1
 
 
 def process_matrix(file, width: int) -> list:
+    # a list containing the groups where the cells of the previous line belong to
+    # the list is updated as the current row is processed
     previous_line_groups = [-1 for _ in range(width)]
+    # the groups list
     groups = []
     current_height = 0
 
@@ -27,12 +30,15 @@ def process_matrix(file, width: int) -> list:
             if row[i] == ACTIVE_ELEMENT:
                 previous_line_group = previous_line_groups[i]
 
+                # if the first element in the row is connected to the above cell
                 if i == 0 and previous_line_group > -1:
                     groups[previous_line_group].append([current_height, i])
 
+                # remaining elements of the row
                 elif i > 0:
                     previous_line_group_previous_element = previous_line_groups[i - 1]
 
+                    # if different groups are connected, join them (consider the group with the lower rank)
                     if -1 < previous_line_group_previous_element != previous_line_group > -1:
 
                         # reassign previously defined group + add current point to group
@@ -48,17 +54,21 @@ def process_matrix(file, width: int) -> list:
 
                         groups[previous_line_groups[i]].append([current_height, i])
 
+                    # if the cell is connected to the previous cell
                     elif previous_line_group_previous_element > -1:
                         previous_line_groups[i] = previous_line_group_previous_element
                         groups[previous_line_group_previous_element].append([current_height, i])
 
+                    # if the cell is connected to the above cell
                     elif previous_line_group > -1:
                         groups[previous_line_group].append([current_height, i])
 
+                    # a new group is created
                     else:
                         groups.append([[current_height, i]])
                         previous_line_groups[i] = len(groups) - 1
 
+            # remove group index
             else:
                 previous_line_groups[i] = -1
 
